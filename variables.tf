125d23fe96e437d540afb5a683ed2896932cfa16
mediDEU <mehdivasef@gmail.com>
2025-06-11 12:52:13 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key" {}
+variable "region" {}
+
+# For Object Storage Bucket
+variable "compartment_id" {}
